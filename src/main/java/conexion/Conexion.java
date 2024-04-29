db6c3be9f8e69f100d47694263face2a24feed28
gontugithub <gs.alvarez8@gmail.com>
2024-04-29 14:08:04 +0200

    creado los metodos con las conexiones de la base de datos

---
+++ src/main/java/conexion/Conexion.java
@@ -0,0 +1,22 @@
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Conexion {
+
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/tienda";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "AlumnoIFP";
+
+    public static Connection open() throws SQLException {
+        Properties props = new Properties();
+
+        props.setProperty("user", USUARIO);
+        props.setProperty("password", PASSWORD);
+        return DriverManager.getConnection(URL, props);
+    }
+
+}
