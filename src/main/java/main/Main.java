aea9183ef3d6bd6343dac6ad32910181a8b2772a
gontugithub <gs.alvarez8@gmail.com>
2024-04-29 14:11:11 +0200

    main creado y menu de seleccion principal del cliente

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,60 @@
+package main;
+
+import entidades.Producto;
+import queries.ProductoQueries;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws SQLException {
+
+
+        int cont = 0;
+        ArrayList<Producto> list = new ArrayList<>();
+
+        do {
+
+            System.out.print(" [1] ORGANIZADO MAS CARO A MAS BARATO\n" +
+                    " [2] ORGANIZADO MAS BARATO A MAS CARO\n" +
+                    " [3] ORGANIZADO ALFABETICAMENTE POR NOMBRE\n" +
+                    " [4] FILTRAR POR CADENA\n" +
+                    " >> ");
+
+            switch (sc.nextInt()){
+
+                case 1:
+                    list = ProductoQueries.getProductosPrecioDesc();
+                    break;
+                case 2:
+                    list = ProductoQueries.getProductosPrecioAsc();
+                    break;
+                case 3:
+                    list = ProductoQueries.getProductosNombreAlfabetico();
+                    break;
+                case 4:
+                    System.out.print("INTRODUCE LA CADENA QUE QUIERES BUSCAR\n >>  ");
+                    list = ProductoQueries.getProductosFiltradoCadena(sc.next());
+                    break;
+                default:
+                    System.out.println("ERROR");
+                    break;
+
+            }
+            System.out.println();
+            for (Producto p : list){
+                System.out.println(p.toString() +"\n");
+            }
+
+        cont++;
+        } while ( cont < 6);
+
+
+
+
+
+    }
+}
